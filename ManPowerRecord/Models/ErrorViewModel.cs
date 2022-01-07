4dd0683612d2fb162a8d8d21e79681ae52a9e0e2
Black - BKP <black.bkp@gmail.com>
2022-01-07 10:26:25 +0700

    Add project files.

---
+++ ManPowerRecord/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ManPowerRecord.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
