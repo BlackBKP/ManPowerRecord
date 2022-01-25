9484527583960d6be5f7b996b76b1c582cf2e47a
Black - BKP <black.bkp@gmail.com>
2022-01-25 17:13:30 +0700

    Add Connection to Database

---
+++ ManPowerRecord/Interfaces/IConnectDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManPowerRecord.Interfaces
+{
+    interface IConnectDB
+    {
+        SqlConnection Connect();
+    }
+}
---
+++ ManPowerRecord/Services/ConnectDB.cs
@@ -0,0 +1,19 @@
+﻿using ManPowerRecord.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManPowerRecord.Services
+{
+    public class ConnectDB : IConnectDB
+    {
+        public SqlConnection Connect()
+        {
+            string connection_string = @"Data Source=localhost\SQLExpress;Initial Catalog=MPR;User ID=sa;Password=p@ssw0rd";
+            SqlConnection connection = new SqlConnection(connection_string);
+            return connection;
+        }
+    }
+}
