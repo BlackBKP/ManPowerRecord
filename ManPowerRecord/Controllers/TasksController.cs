6fa0c3f31ccf7a884c8cd24c5a8cff804c2403c7
Black - BKP <black.bkp@gmail.com>
2022-01-26 12:14:10 +0700

    Create Task View, Add Task

---
+++ ManPowerRecord/Controllers/TasksController.cs
@@ -0,0 +1,37 @@
+﻿using ManPowerRecord.Interfaces;
+using ManPowerRecord.Models;
+using ManPowerRecord.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManPowerRecord.Controllers
+{
+    public class TasksController : Controller
+    {
+        IJob JobService;
+        ITask TaskService;
+
+        public TasksController()
+        {
+            this.JobService = new JobService();
+            this.TaskService = new TaskService();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AddTask(string task_string)
+        {
+            TaskModel task = JsonConvert.DeserializeObject<TaskModel>(task_string);
+            var result = TaskService.CreateTask(task);
+            return Json(result);
+        }
+    }
+}
---
+++ ManPowerRecord/Services/TaskService.cs
@@ -0,0 +1,40 @@
+﻿using ManPowerRecord.Interfaces;
+using ManPowerRecord.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManPowerRecord.Services
+{
+    public class TaskService : ITask
+    {
+        IConnectDB Database;
+
+        public TaskService()
+        {
+            this.Database = new ConnectDB();
+        }
+
+        public string CreateTask(TaskModel task)
+        {
+            this.Database = new ConnectDB();
+            SqlConnection connection = Database.Connect();
+            connection.Open();
+            using(SqlCommand command = new SqlCommand("INSERT INTO Tasks(task_id, task_name, task_description, job_id, status) " +
+                                                      "VALUE(@task_id, @task_name, @task_description, @job_id, @status)", connection))
+            {
+                command.CommandType = System.Data.CommandType.Text;
+                command.Connection = connection;
+                command.Parameters.AddWithValue("@task_id", task.task_id);
+                command.Parameters.AddWithValue("@task_name", task.task_name);
+                command.Parameters.AddWithValue("@task_description", task.task_description);
+                command.Parameters.AddWithValue("@job_id", task.job_id);
+                command.Parameters.AddWithValue("@status", task.status);
+            }
+            connection.Close();
+            return "Success";
+        }
+    }
+}
